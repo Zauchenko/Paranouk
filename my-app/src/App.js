c02090cf42420083e50fb9c0360c39d0b351a548
Zauchenko <zov.radek@gmail.com>
2020-12-01 16:55:33 +0200

    18 lesson finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>);
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            {/*<Profile />*/}
+            <div className = 'app-wrapper-content' >
+            <Dialogs />
+            </div>
+        </div>);
 }
-
-
-
-
-
-
 
 
 export default App;
