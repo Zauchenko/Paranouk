a68d46d9b784769b32ff495346cf0632bc944dca
Zauchenko <zov.radek@gmail.com>
2021-05-11 15:08:27 +0300

    64 lesson done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -34,24 +34,26 @@ let Users = (props) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {
-
+                                ? <button disabled={props.followingInProgress.some(id => id=== u.id)} onClick={() => {
+                                    props.toggleFollowingProgress(true, u.id);
                                     usersAPI.unfollowUsers(u.id)
                                         .then(data => {
                                             if (data.resultCode === 0) {
                                                 props.unfollow(u.id);
                                             }
+                                            props.toggleFollowingProgress(false, u.id);
                                         });
 
 
                                 }}>Unfollow</button>
-                                : <button onClick={() => {
-
+                                : <button disabled={props.followingInProgress.some(id => id=== u.id)} onClick={() => {
+                                    props.toggleFollowingProgress(true, u.id);
                                     usersAPI.followUsers(u.id)
                                         .then(data => {
                                             if (data.resultCode === 0) {
                                                 props.follow(u.id);
                                             }
+                                            props.toggleFollowingProgress(false, u.id);
                                         });
 
                                 }}>Follow</button>}
--- src/components/Users/Userscontainer.jsx
+++ src/components/Users/Userscontainer.jsx
@@ -5,9 +5,9 @@ import {
     setCurrentPage,
     setTotalUsersCount,
     setUsers,
-    unfollow
+    unfollow, toggleFollowingProgress
 }
-from '../../Redux/users-reducer';
+    from '../../Redux/users-reducer';
 import {connect} from 'react-redux';
 import Users from './Users';
 import Preloader from "../Common/Preloader/Preloader";
@@ -48,7 +48,8 @@ class Userscontainer extends React.Component {
                    users={this.props.users}
                    follow={this.props.follow}
                    unfollow={this.props.unfollow}
-
+                   followingInProgress={this.props.followingInProgress}
+                   toggleFollowingProgress={this.props.toggleFollowingProgress}
             />
         </>
     }
@@ -61,11 +62,12 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
     }
 }
 
 
 export default connect(mapStateToProps, {
-    follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching
+    follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress
 })(Userscontainer);
