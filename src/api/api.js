01fe148ec630e3e36177e5817009d26beb0d1f59
Zauchenko <zov.radek@gmail.com>
2021-05-14 13:46:05 +0300

    75 lesson done

--- src/api/api.js
+++ src/api/api.js
@@ -54,4 +54,23 @@ export const profileAPI = {
 }
 
 
+// export const authAPI = {
+//
+//     getProfileId(userId) {
+//         return instance.get(`profile/` + userId)
+//             .then(response => response.data)
+//     },
+//     getStatus(userId) {
+//         return instance.get(`profile/status/` + userId)
+//             .then(response => response.data)
+//     },
+//     updateStatus(status) {
+//         return instance.put(`profile/status/`, {status: status})
+//             .then(response => response.data)
+//     }
+//
+//
+// }
+
+
 
--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={"Login"} name={"login"} component={"input"}/>
+                </div>
+                <div>
+                    <Field placeholder={"Password"} name={"password"} component={"input"}/>
+                </div>
+                <div>
+                    <Field component={"input"} type={"checkbox"} name={"remember me"}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+const  onSubmit = (formData) => {
+
+}
 
 const Login = (props) => {
-    return <h1>Login</h1>
+    return (
+        <div>
+
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    )
 }
 
 
